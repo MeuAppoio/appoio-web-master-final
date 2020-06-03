609e6a4a1d59bf323ca3093c668e26a615be3323
Wesley Castilho <onsite.webdev@gmail.com>
2020-06-03 18:35:45 -0300

    Ajuste de header

--- vue.config.js
+++ vue.config.js
@@ -26,7 +26,7 @@ module.exports = {
   // Configure Webpack's dev server.
   // https://cli.vuejs.org/guide/cli-service.html
   devServer: {
-    disableHostCheck: true,
+    public: 'meuappoio.com.br:8080',
     ...(process.env.API_BASE_URL
       ? // Proxy API endpoints to the production base URL.
         { proxy: { '/api': { target: process.env.API_BASE_URL } } }
